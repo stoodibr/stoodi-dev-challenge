197423697e65d6bc7ef1f750822997dee1871d54
Guilherme Viegas <guilherme.viegas@amopromo.com>
2020-11-08 10:10:05 -0300

    Sotory 1 - Create Question and Answer models

--- question/models.py
+++ question/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.TextField(
+        verbose_name="Questão",
+        help_text="Enunciado da questão."
+    )
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        'question.Question',
+        related_name='answers',
+        on_delete = models.CASCADE
+    )
+    letter = models.CharField(
+        max_length=1,
+        verbose_name='Opção (letra)'
+    )
+    value = models.TextField(
+        verbose_name='Texto da resposta'
+    )
+
