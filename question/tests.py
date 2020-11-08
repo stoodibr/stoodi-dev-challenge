2c265abd839990fc15668f106e75632e15d3063c
Guilherme Viegas <guilherme.viegas@amopromo.com>
2020-11-08 11:52:09 -0300

    Check ordering of answers

--- question/tests.py
+++ question/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Question
 
 # Create your tests here.
+class QuestionTestCase(TestCase):
+
+    __question = None
+    __text = 'Quanto é 2^5?'
+    __answers = {
+        'b': '2',
+        'a': '0',
+        'c': '16',
+        'e': '128',
+        'd': '32',
+    }
+
+    def setUp(self):
+
+        self.__question = Question.objects.create(title=self.__text)
+
+        for answer in self.__answers:
+            self.__question.add_answer(answer, self.__answers[answer])
+
+    def test_sorted_answers(self):
+        self.assertEqual(list(Question.objects.last().get_answers().keys()), list(sorted(self.__answers.keys())))
+
+
+
+
+
+
