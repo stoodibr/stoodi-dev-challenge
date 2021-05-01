a7bf35ba9a12365b00f87f2c6b0d8f2edd0cb242
fernandamerli <fermerli.97@gmail.com>
2021-05-01 14:17:14 -0300

    feat: getting questions and answers from db

--- question/urls.py
+++ question/urls.py
@@ -2,10 +2,12 @@
 
 from django.urls import path, re_path
 
-from . import views
+from question.views import Question
+
+q = Question()
 
 urlpatterns = [
-    re_path(r'^$', views.question, name='question'),
-    re_path(r'^resposta/$', views.question_answer, name='question_answer'),
+    re_path(r'^$', q.question, name='question'),
+    re_path(r'^resposta/$', q.question_answer, name='question_answer'),
 
 ]
--- question/views.py
+++ question/views.py
@@ -2,33 +2,62 @@
 import collections
 
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from question.models import Answer as AnswerModel, Question as QuestionModel
 
 
-def question(request):
-    text = 'Quanto é 2^5?'
+class Question:
 
-    answers = {
-        'a': '0',
-        'b': '2',
-        'c': '16',
-        'd': '32',
-        'e': '128',
-    }
-    sorted_answers = collections.OrderedDict(sorted(answers.items()))
+    def __init__(self):
+        self.set_questions_list()
 
-    context = {
-        'question_text': text,
-        'answers': sorted_answers,
-    }
 
-    return render(request, 'question/question.html', context=context)
+    def set_questions_list(self):
+        q = QuestionModel.objects.all().values()
 
-def question_answer(request):
-    answer = request.POST.get('answer', 'z')
-    is_correct = answer == 'd'
+        questions_list = []
+        for question in q:
+            a = AnswerModel.objects.filter(question_ref=question.get('id')).values()
+            answers = {}
+            for answer in a:
+                answers.update({answer['choice_item']: answer['response']})
+            sorted_answers = collections.OrderedDict(sorted(answers.items()))
 
-    context = {
-        'is_correct': is_correct,
-    }
+            correct_answer_id = question.get('correct_answer_id')
+            correct_answer = AnswerModel.objects.filter(id=correct_answer_id).values()[0]
 
-    return render(request, 'question/answer.html', context=context)
+            questions_list.append({
+                'question_text': question.get('text'),
+                'correct_answer': correct_answer,
+                'answers': sorted_answers,
+            })
+        
+        self.paginator = Paginator(questions_list, 1)
+
+
+    def question(self, request):
+        self.page = request.GET.get('page') or 1
+        question = self.paginator.get_page(self.page)
+
+        context = {
+            'page': self.page,
+            'question': question
+        }
+
+        return render(request, 'question/question.html', context=context)
+
+
+    def question_answer(self, request):
+        answer = request.POST.get('answer', 'z')
+        question = self.paginator.get_page(self.page)
+        correct_answer = question.object_list[0].get('correct_answer')
+
+        is_correct = answer == correct_answer.get('choice_item')
+
+        context = {
+            'is_correct': is_correct,
+            'question': question
+        }
+
+        return render(request, 'question/answer.html', context=context)
