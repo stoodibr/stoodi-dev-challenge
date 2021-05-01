e303d22002e9e64e9e4bdcd4e49ba1bd7e758262
fernandamerli <fermerli.97@gmail.com>
2021-05-01 13:59:06 -0300

    feat: creating question and answer models

--- question/admin.py
+++ question/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from question.models import Answer, Question
+
+
+admin.site.register(Answer)
+admin.site.register(Question)
--- question/models.py
+++ question/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=100)
+    correct_answer = models.ForeignKey('Answer', on_delete=models.CASCADE)
+
+
+class Answer(models.Model):
+    question_ref = models.ForeignKey('Question', on_delete=models.CASCADE)
+    choice_item = models.CharField(max_length=1)
+    response = models.CharField(max_length=100)
