87b257d2f1b2604ad1d1292c08ddf588015217f2
gabriel franchi <gabriel.franchi.gf@gmail.com>
2021-05-10 22:01:53 -0300

    Added logs for logged users

--- selecao/urls.py
+++ selecao/urls.py
@@ -12,11 +12,16 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  path(r'^blog/', include('blog.urls'))
 """
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.contrib import admin
-
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^', include('question.urls')),
+    path('cadastro/', user_views.register, name='register'),
+    path('log-questoes/', user_views.user_log, name='user_log'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', include('question.urls')),
 ]
