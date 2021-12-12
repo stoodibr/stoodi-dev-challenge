433008d849df3ea3e25a04c631339fee3d5de098
Luciano Feder <lucianoo.feder@gmail.com>
2021-12-11 21:18:03 -0300

    historia_1::feat: created models for Question and QuestionChoices

--- question/models.py
+++ question/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    text = models.TextField()
+    correct_answer = models.OneToOneField('question.QuestionChoice', on_delete=models.CASCADE, related_name="answers_question", null=True)
+
+
+class QuestionChoice(models.Model):
+    question = models.ForeignKey('question.Question', on_delete=models.CASCADE, related_name='answers')
+    text = models.TextField()
+    label = models.CharField(max_length=1)
