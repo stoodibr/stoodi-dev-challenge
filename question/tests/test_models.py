c866d732a53e640b572956beafd0d7904d5bb088
Luciano Feder <lucianoo.feder@gmail.com>
2021-12-13 19:32:45 -0300

    historia_5::tests: created test for QuestionRecord model

--- question/tests/test_models.py
+++ question/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from question.models import Question, QuestionChoice
+from question.models import Question, QuestionChoice, QuestionRecords
 
 class TestQuestionModel(TestCase):
     @classmethod
@@ -28,12 +28,6 @@ class TestQuestionChoiceModel(TestCase):
         cls.question = Question.objects.create(
             text=cls.text,
         )
-
-        pergunta = {
-            "text": '2+2 eh ?',
-            "correct_answer": 'C',
-            "answers": ['1','3','4']
-        }
 
         cls.answers = [
             QuestionChoice.objects.create(
@@ -70,3 +64,56 @@ class TestQuestionChoiceModel(TestCase):
     def test_correct_answer_relationship(self):
         self.assertEqual(self.answers[1].answers_question, self.question)
         self.assertEqual(self.question.correct_answer, self.answers[1])
+
+
+
+class TestQuestionRecordModel(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.text = 'Question title'
+
+        cls.question = Question.objects.create(
+            text=cls.text,
+        )
+
+        cls.answers = [
+            QuestionChoice.objects.create(
+                question=cls.question,
+                text=f'Text option {i}',
+                label=f'{chr(ord("A") + i)}'
+            ) for i in range(4)
+        ]
+
+        cls.question.correct_answer = cls.answers[1]
+
+        cls.question.save()
+
+        cls.question_record = QuestionRecords.objects.create(question=cls.question, answered='B')
+        cls.question_record.save()
+
+
+    def test_create_question_record_success(self):
+        self.assertIsNotNone(self.question_record.id)
+
+
+    def test_question_record_fields(self):
+        
+        self.assertIsInstance(self.question_record.question, Question)
+
+        self.assertIsInstance(self.question_record.answered, str)
+        self.assertEqual(self.question_record.answered, 'B')
+
+        self.assertIsInstance(self.question_record.is_correct_answered, bool)
+        self.assertEqual(self.question_record.is_correct_answered, True)
+
+    def test_correct_answer_field_false(self):
+        question_record = QuestionRecords.objects.create(question=self.question, answered='D')
+        question_record.save()
+
+        self.assertEqual(question_record.is_correct_answered, False)
+
+
+    def test_question_choices_relationship(self):
+        self.assertEqual(self.question.records.count(), 1)
+
+
