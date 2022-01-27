187b4f6d71e5432e9d9e0c3bfbc596cb66610fc6
isTorres <ismaeltorres612@gmail.com>
2022-01-27 01:24:37 -0300

    feat: test view questions

--- question/tests.py
+++ question/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestQuestionView(TestCase):
+	def test_view_status(self):
+		response = self.client.get(reverse('question:question'))
+		text = 'Quanto é 2^5?'
+		answers = {
+			'a': '0',
+			'b': '2',
+			'c': '16',
+			'd': '32',
+			'e': '128',
+		}
+
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response.context['question_text'], text)
+		self.assertContains(response.context['answers'], answers)
