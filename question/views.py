69bd78aa4a566f28c7db20f3c33939b919520d6b
alexlopesbr <criabio@hotmail.com>
2022-02-11 07:15:33 -0300

    feat: question and answer pagination

--- question/views.py
+++ question/views.py
@@ -1,32 +1,47 @@
-#coding: utf8
+# coding: utf8
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Question, Answer
 
 
 def question(request):
-    text = 'Quanto é 2^5?'
-
-    # BUG: as respostas estão ficando fora de ordem
-    answers = {
-        'a': '0',
-        'b': '2',
-        'c': '16',
-        'd': '32',
-        'e': '128',
-    }
+    answers = Answer.objects.all()
 
     context = {
-        'question_text': text,
         'answers': answers,
+        'questions': questions_pagination(request),
     }
 
     return render(request, 'question/question.html', context=context)
 
+
 def question_answer(request):
-    answer = request.POST.get('answer', 'z')
-    is_correct = answer == 'd'
+    answer = request.POST.get('answer', 'False').split(',')
+    user_answer = answer[0]
+    current_page = answer[1]
+
+    is_correct = user_answer == 'True'
 
     context = {
         'is_correct': is_correct,
+        'questions': question_answer_pagination(current_page)
     }
 
     return render(request, 'question/answer.html', context=context)
+
+
+def questions_pagination(request):
+    questions = Question.objects.all()
+    paginator = Paginator(questions, 1)
+    page = request.GET.get('question')
+    questions = paginator.get_page(page)
+
+    return questions
+
+
+def question_answer_pagination(current_page):
+    questions = Question.objects.all()
+    paginator = Paginator(questions, 1)
+    questions = paginator.get_page(current_page)
+
+    return questions
