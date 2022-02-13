8bc42056a76995b30d7b348c5c8a0fe9a68419e7
pierre fenner <pierre.fenner@gestaods.com.br>
2022-02-13 02:25:27 -0300

    historia 1: models Pergunta

--- question/models.py
+++ question/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class Pergunta(models.Model):
+    A = "A"
+    B = "B"
+    C = "C"
+    D ="D"
+    E ="E"
+    
+    CHOICES = (
+        (A, "A"),
+        (B, "B"),
+        (C, "C"),
+        (D, "D"),
+        (E, "E")
+    )
+    
+
+    texto = models.TextField('Texto da questão', null=True)
+    opcao_A = models.CharField("Opção A", max_length=50, null=True)
+    opcao_B = models.CharField("Opção B", max_length=50, null=True)
+    opcao_C = models.CharField("Opção C", max_length=50, null=True)
+    opcao_D = models.CharField("Opção D", max_length=50, null=True)
+    opcao_E = models.CharField("Opção E", max_length=50, null=True)
+    resposta =  models.CharField(
+        u"Resposta", choices=CHOICES, max_length=20, blank=True, null=True
+    )
+
+    data_cadastro = models.DateTimeField(auto_now=True)
+    #opcaos = models.ManyToManyField(Resposta)
+    class Meta:
+        verbose_name = "Pergunta"
+        verbose_name_plural = "Perguntas"
+
+    def __str__(self):
+        return "%s" % (self.texto)
