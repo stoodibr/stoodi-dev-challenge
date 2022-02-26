d83d4928c87442760fb3d43431d814a6a64599ed
Rafael <rafael.matthes@live.com>
2022-02-26 00:58:51 -0300

    feat: try again / next question

--- question/classes/questions_class.py
+++ question/classes/questions_class.py
@@ -4,10 +4,11 @@ class QuestionsRequest():
 
     def __init__(self, id = None):
         self.__context = self.__get_questions_context(id)
+        self.__current_answer = None
 
     def __get_questions_context(self, id_question):
 
-        question = Question.objects.get(id=id_question) if id_question else Question.objects.all()[0]
+        question = Question.objects.get(id=id_question) if Question.objects.filter(id=id_question).exists() else Question.objects.all()[0]
         answer_list = Answer.objects.filter(question_id=question.id)[:5]
     
         if answer_list:
@@ -16,8 +17,24 @@ class QuestionsRequest():
                 'answers': answer_list,
             }
         
-        raise ValueError('A pergunta solicitada ainda não está disponível')
+    def get_next_question_id(self):
+        
+        if self.__current_answer:
+            next_question_id = int(self.__current_answer.question_id.id) + 1
+            if Question.objects.filter(id=next_question_id).exists():
+                return str(next_question_id)
+        
+        return Question.objects.all()[0].id
+
+    def get_question_result(self, id_answer):
+        if id_answer:
+            self.__current_answer =  Answer.objects.get(id=id_answer)
+            return self.__current_answer.is_correct
+    
+    
+    def get_current_question(self):
+        if self.__current_answer:
+            return self.__current_answer.question_id.id
     
     def get_context(self):
-
         return self.__context
--- question/views.py
+++ question/views.py
@@ -1,35 +1,11 @@
 #coding: utf8
 from django.shortcuts import render
-from .models import Question, Answer
 from .classes.questions_class import *
-
-# class QuestionsRequest():
-
-#     def __init__(self, id = None):
-#         self.__context = self.__get_questions_context(id)
-
-#     def __get_questions_context(self, id_question):
-
-#         question = Question.objects.get(id=id_question) if id_question else Question.objects.all()[0]
-#         answer_list = Answer.objects.filter(question_id=question.id)[:5]
-    
-#         if answer_list:
-#             return {
-#                 'question_text': question.text,
-#                 'answers': answer_list,
-#             }
-        
-#         raise ValueError('A pergunta solicitada ainda não está disponível')
-    
-#     def get_context(self):
-
-#         return self.__context
 
 def question(request, id = None):
 
     try:
         context = QuestionsRequest(id).get_context()
-
     except Exception as e:
         context = {
             'error_msg' : str(e)
@@ -39,31 +15,22 @@ def question(request, id = None):
 
 def question_answer(request):
     
-    answer, is_correct = request.POST.get('answer', None), False
-    
-    if answer:
-        is_correct = Answer.objects.get(id=answer).is_correct
+    questions = QuestionsRequest()
+    try:
+        answer, is_correct = request.POST.get('answer', None), False
+        if answer:
+            is_correct = questions.get_question_result(answer)
 
-    context = {
-        'is_correct': is_correct,
-    }
+        context = {
+            'is_correct': is_correct,
+        }
+    
+    except Exception as e:
+        context = {
+            'error_msg' : str(e)
+        }
+
+    context['next_question'] = questions.get_next_question_id()
+    context['current_question'] = questions.get_current_question()
 
     return render(request, 'question/answer.html', context=context)
-
-
-# def question(request):
-   
-#     context = QuestionsRequest().get_context()
-#     context['teste'] = request.GET.get('id', 'errow')
-
-#     return render(request, 'question/question.html', context=context)
-
-# def question_answer(request):
-#     answer = request.POST.get('answer', 'z')
-#     is_correct = answer == 'd'
-
-#     context = {
-#         'is_correct': is_correct,
-#     }
-
-#     return render(request, 'question/answer.html', context=context)
