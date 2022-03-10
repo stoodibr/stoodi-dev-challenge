911e9853c6efa25df67738bafff87a08c427b9c2
MatheusLFumian <matheusfumian@gmail.com>
2022-03-10 18:10:24 -0300

    Historia1: Criaçao dos models necessarios e ativaçao no painel administrativo do Django para facilitar a inserçao das perguntas

--- question/models.py
+++ question/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=300)
+    correct_answer = models.CharField(max_length=4)
+
+class Answers(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    answer_option = models.CharField(max_length=4)
