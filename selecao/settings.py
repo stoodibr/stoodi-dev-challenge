8686725da24d8b18e46c5c9118b4ea1772f02124
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2022-03-21 00:05:31 -0300

    feat(historia6): criar model custom user para utilizar email em vez de username

--- selecao/settings.py
+++ selecao/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     'question',
     'user'
 )
@@ -60,6 +61,7 @@ SILENCED_SYSTEM_CHECKS = [
 
 ROOT_URLCONF = 'selecao.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -71,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+            ]
         },
     },
 ]
@@ -108,3 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# auth
+AUTH_USER_MODEL = 'user.CustomUser'
--- user/models.py
+++ user/models.py
@@ -1,6 +1,9 @@
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from question.models import Question
+from user.managers import CustomUserManager
 
 
 class UserHistory(models.Model):
@@ -17,3 +20,16 @@ class UserHistory(models.Model):
             'is_correct': self.is_correct,
             'datetime': self.created_at
         })
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
