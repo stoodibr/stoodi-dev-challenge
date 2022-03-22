0d610c598284a72ba4adaded7a96c7dd8f0cd178
Fabíola Fleury <fabiola.mfleury@gmail.com>
2022-03-22 17:05:01 -0300

    H1 - Adiciona opção 'e' na model Question

--- question/models.py
+++ question/models.py
@@ -4,7 +4,8 @@ CORRECT_OPTION_CHOICES = (
         ('A', 'Option A'),
         ('B', 'Option B'),
         ('C', 'Option C'),
-        ('D', 'Option D')
+        ('D', 'Option D'),
+        ('E', 'Option E'),
     )
 
 class Question(models.Model):
@@ -13,6 +14,7 @@ class Question(models.Model):
     option_b = models.CharField(max_length=200)
     option_c = models.CharField(max_length=200)
     option_d = models.CharField(max_length=200)
+    option_e = models.CharField(max_length=200)
 
     correct_option = models.CharField(max_length=1, choices=CORRECT_OPTION_CHOICES)
 
