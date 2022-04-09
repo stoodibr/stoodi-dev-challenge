447823ef1f7c0cba7e8fa3990199de08a3383f11
Cezar Augusto Barbosa de Souza <cezaraugusto0406@gmail.com>
2022-04-09 02:32:24 -0400

    feat(api): create models and migrations

--- question/models.py
+++ question/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    description = models.TextField()
+    correct_alternative = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.description
+
+class Answer(models.Model): 
+    alternative = models.CharField(max_length=1)
+    description = models.TextField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
