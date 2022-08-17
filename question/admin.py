d4807148956cc0e045c88a1bc549ea9d0f0fa3d9
Artur Felipe Veloso <arturfdasveloso@gmail.com>
2022-08-17 16:26:46 -0300

    History 1: Creating the tables of Questions and Alternatives

--- question/admin.py
+++ question/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Question, Alternatives
 
-# Register your models here.
+# Register admin models
+admin.site.register(Question)
+
+@admin.register(Alternatives)
+class alternativesAdmin(admin.ModelAdmin):
+    list_display = (
+        'alternative_order',
+        'question',
+        'alternative_text',
+        'is_correct'
+    )
--- question/models.py
+++ question/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+# Models
+class Question(models.Model):
+    question_text = models.TextField(verbose_name="Questão")
+    
+    def __str__(self):
+        return self.question_text
+
+    class Meta:
+        ordering = ('pk',)
+        verbose_name = 'Questão'
+        verbose_name_plural = 'Questões'
+        
+class Alternatives(models.Model):
+    alternative_order = models.CharField(max_length=4, verbose_name="Alternativa")
+    question = models.ForeignKey(
+        'Question', 
+        on_delete=models.CASCADE, 
+        verbose_name="Questão"
+    )
+    alternative_text = models.TextField(verbose_name="Questão")
+    is_correct = models.BooleanField(
+        verbose_name="Está correta?", 
+        default=False
+    )
+    
+    def __str__(self):
+        return self.alternative_text
+
+    class Meta:
+        ordering = ('alternative_order',)
+        verbose_name = 'Alternativa'
+        verbose_name_plural = 'Alternativas'
