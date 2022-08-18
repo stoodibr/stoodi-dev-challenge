5fb3f33858dc3cda748dc366b30cba1d6b1cd0f4
Tiago Vilardi <tiagoamx@gmail.com>
2022-08-17 23:37:31 -0300

    Implementacao da url de login/logout utilizando ClassBasedViews padrão do django e implementando templates de login.
    Ajuste nas paginas para conter os dados do usuario logado e links para autenticacao caso deslogado.

--- question/urls.py
+++ question/urls.py
@@ -1,11 +1,17 @@
 #coding: utf8
 
 from django.urls import path, re_path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.question, name='question'),
-    path('<uuid:questao_identficador>', views.question, name='question_idenficated'),
-    re_path(r'^resposta/$', views.question_answer, name='question_answer'),
+    path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    
+    path('', views.question, name='question'),
+    path('<uuid:questao_identficador>', 
+        views.question, name='question_idenficated'),
+    path('resposta/', 
+        views.question_answer, name='question_answer'),
 ]
