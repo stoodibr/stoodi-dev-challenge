6513324ef43b83dc7d7fdb77eb09eeeeb6797f09
Tiago Vilardi <tiagoamx@gmail.com>
2022-08-18 11:10:06 -0300

    Upgrade de versao do django para 4.0.*, ou seja a mais recente dentro da versao 4.0. do framework.
    A versão 4.0 do django foi selecionada devido a versão mais recente e compativel com a lib pytest-django na versão 4.5.2. utilizada nos testes unitários.

--- selecao/settings.py
+++ selecao/settings.py
@@ -54,11 +54,6 @@ MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-SILENCED_SYSTEM_CHECKS = [
-    'admin.E408',
-    'admin.E409',
-    'admin.E410',
-]
 
 ROOT_URLCONF = 'selecao.urls'
 
@@ -101,8 +96,6 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
 
@@ -110,3 +103,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
